93ad2014f26129b2777a7a20b283fd29206c2aab
EDI <yao_lk@163.com>
2020-06-30 09:59:12 +0800

    增加Swagger配置

---
+++ src/main/java/com/springbootplus/core/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.springbootplus.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))                         //这里采用包含注解的方式来确定要显示的接口
+                // .apis(RequestHandlerSelectors.basePackage("com.pactera.modular.system.controller"))    //这里采用包扫描的方式来确定要显示的接口 
+                .build();
+    }
+
+    /**
+     * 该套 API 说明，包含作者、简介、版本、host、服务URL
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+        		.license("Apache License Version 2.0")
+                .title("SpringBootPlus")
+                .description("API接口文档")
+                .version("2.0")
+                .build();
+    }
+}
