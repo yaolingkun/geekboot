a811241dd2e3bf6b89543fc6ecb6bbc4ef595df2
EDI <yao_lk@163.com>
2020-07-03 16:46:31 +0800

    优化

--- src/main/java/com/springbootplus/core/util/StringUtils.java
+++ src/main/java/com/springbootplus/core/util/StringUtils.java
@@ -6,6 +6,74 @@ public class StringUtils {
 	 * @return
 	 */
 	public static String getSingleVerticalLine(){
-		return " | ";
+		return "|";
+	}
+	/**
+	 * 判断字符串是否为不空
+	 * @param arg
+	 * @return
+	 */
+	public static boolean isNotEmpty(StringBuffer  arg){
+		if( null == arg || arg.toString().isEmpty()){
+			return false ;
+		}
+		return true;
+	}
+	
+	/**
+	 * 判断字符串是否为空
+	 * @param arg
+	 * @return
+	 */
+	public static boolean isEmpty(StringBuffer  arg){
+		return !isNotEmpty(arg);
+	}
+	
+	/**
+	 * 判断字符串是否为不空
+	 * @param arg
+	 * @return
+	 */
+	public static boolean isNotEmpty(String  arg){
+		if( null == arg || arg.toString().isEmpty()){
+			return false ;
+		}
+		return true;
+	}
+	
+	/**
+	 * 判断字符串是否为空
+	 * @param arg
+	 * @return
+	 */
+	public static boolean isEmpty(String  arg){
+		return !isNotEmpty(arg);
+	}
+	
+	
+	
+	
+	public static String argToLowerCase(String arg){
+		if(isNotEmpty(arg))
+		return arg.toLowerCase();
+		return arg;
+	}
+	
+	/**
+	 * url为空、url包含过滤(不需要拦截)的字符串返回false
+	 * @param url
+	 * @return
+	 */
+	public static boolean importantURL(StringBuffer url){
+		if(StringUtils.isEmpty(url)){
+			return false;
+		}
+		String[] excludeField = {"swagger","error"};
+		for(String item : excludeField){
+			if(url.toString().contains(item)){
+				return false;
+			}
+		}
+		return true;
 	}
 }
